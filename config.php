f52f7cf4a4baf248a0a8767df8c0518e7642b69e
OhIamrajan <96912564+OhIamrajan@users.noreply.github.com>
2022-02-19 00:41:28 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+/*
+oh shit 
+what do to 
+*/
+
+define('DB_SERVER','localhost');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+define('DB_NAME','login');
+
+//connecting the database
+
+$conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
+
+if($conn == false){
+    dir('Error: Cannot connect');
+}
+
+?>
